7c77e5580d185c1ecc86451f1acd0c33bd55b56c
Michael Wydra <wydrama@gmail.com>
2022-06-24 18:46:37 -0500

    added second dice

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,8 +13,24 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        val dice2  = Dice(6);
+        val diceRoll2 = dice2.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll1.toString()
+        val resultTextView2: TextView = findViewById(R.id.textView4)
+        resultTextView2.text = diceRoll2.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
