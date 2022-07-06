1d4624652217e1aab14981107de3c43dde2e5dc2
Michael Wydra <wydrama@gmail.com>
2022-07-05 22:37:59 -0500

    added initial dice roll and added comments

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,12 +16,21 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
-    }
 
+        rollDice();
+    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice1 = Dice(6)
         val diceRoll = dice1.roll()
+
+        // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine which drawable resource ID to use based on the dice roll
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -30,7 +39,11 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+
+        // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+
+        // Update the content description for screen readers
         diceImage.contentDescription = diceRoll.toString()
     }
 }
