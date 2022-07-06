10011c04409907e6cb3bd1a6078e75169bfb3822
Michael Wydra <wydrama@gmail.com>
2022-07-05 22:03:38 -0500

    sample dice image shows on button press

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -20,12 +21,8 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice1 = Dice(6)
         val diceRoll1 = dice1.roll()
-        val dice2  = Dice(6);
-        val diceRoll2 = dice2.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll1.toString()
-        val resultTextView2: TextView = findViewById(R.id.textView4)
-        resultTextView2.text = diceRoll2.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
